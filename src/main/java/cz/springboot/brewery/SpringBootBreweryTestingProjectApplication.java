eb91617ec2240eea7760d12869ae4e41e4bcf393
Martin <MartinVahala@seznam.cz>
2023-02-08 15:18:39 +0100

    Spring Boot Testing Brewery project - initial commit

---
+++ src/main/java/cz/springboot/brewery/SpringBootBreweryTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package cz.springboot.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBreweryTestingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBreweryTestingProjectApplication.class, args);
+	}
+
+}
