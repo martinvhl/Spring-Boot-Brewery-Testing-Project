08c8a3c2289d567d6bd0d9cbfcdc9c2f6e2bedf3
Martin <MartinVahala@seznam.cz>
2023-02-09 10:33:24 +0100

    Error resolved - contextLoads test OK

--- src/test/java/cz/springboot/brewery/SpringBootBreweryTestingProjectApplicationTests.java
+++ src/test/java/cz/springboot/brewery/SpringBootBreweryTestingProjectApplicationTests.java
@@ -3,6 +3,7 @@ package cz.springboot.brewery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//JUnit 5 test (žádné @RunWith(SpringRunner.class)) není potřeba
 @SpringBootTest
 class SpringBootBreweryTestingProjectApplicationTests {
 
